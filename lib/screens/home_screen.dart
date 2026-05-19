012925c56eec8857490ed2780ca368130dd5f3d6
ShaunStrudwick <shaunstrudwick@gmail.com>
2026-05-19 17:27:31 +0100

    Add HomeScreen with navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'clients_screen.dart';
+import 'workflows_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final _screens = const [
+    ClientsScreen(),
+    WorkflowsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FlowPilot')),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() => _selectedIndex = index);
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.people_outline),
+            label: 'Clients',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.route_outlined),
+            label: 'Workflows',
+          ),
+        ],
+      ),
+    );
+  }
+}
