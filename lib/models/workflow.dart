14472271dbd712fc92bdc6e8f92a7613d59084dd
ShaunStrudwick <shaunstrudwick@gmail.com>
2026-05-19 17:20:36 +0100

    Add Workflow model

---
+++ lib/models/workflow.dart
@@ -0,0 +1,13 @@
+class Workflow {
+  final String id;
+  final String name;
+  final String description;
+  final List<String> steps;
+
+  Workflow({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.steps,
+  });
+}
